c1df6d2a50585172e04649c68b21405ce97b3f33
Jennifer Balseca <jibalseca@puce.edu.ec>
2026-06-17 15:50:29 -0500

    Creación de repositorios y perfil de usuario + css

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,5 +1,5 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonButton, IonContent, IonHeader, IonInput, IonPage, IonTextarea, IonTitle, IonToolbar } from '@ionic/react';
+
 import './Tab2.css';
 
 const Tab2: React.FC = () => {
@@ -7,16 +7,46 @@ const Tab2: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Formulario de Repositorio</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
+            <IonTitle size="large">Formulario de Repositorio</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+
+        <div className="form-container">
+          <IonInput
+          className="form-field"
+          label="Nombre del Repositorio"
+          labelPlacement="floating"
+          placeholder="Ingrese el nombre del Repositorio"
+        />
+
+        <IonTextarea
+          className="form-field"
+          label="Descripción del Repositorio"
+          labelPlacement="floating"
+          placeholder="Ingrese el descripción del Repositorio"
+          rows={6}
+        />
+
+        <IonButton
+          className="form-field"
+          expand="block"
+          shape="round"
+          color="primary"
+        >
+          Guardar
+        </IonButton>
+        </div>
+    
+
+
+
+
       </IonContent>
     </IonPage>
   );
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,5 +1,5 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+
 import './Tab3.css';
 
 const Tab3: React.FC = () => {
@@ -7,16 +7,30 @@ const Tab3: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Perfil de Usuario</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
+            <IonTitle size="large">Perfil de Usuario</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+
+        <div className="card-container">
+          <IonCard className="card">
+            <img src="https://avatars.githubusercontent.com/u/232135555?v=4&size=64" alt="Avatar"/>
+            <IonCardHeader>
+              <IonCardTitle>Jennifer Balseca</IonCardTitle>
+              <IonCardSubtitle>jenniferbalseca</IonCardSubtitle>
+            </IonCardHeader>
+            <IonCardContent>
+              <p>Desarrolladora de Software</p>
+            </IonCardContent>
+          </IonCard>
+
+        </div>
+
       </IonContent>
     </IonPage>
   );
